3c3fe093a4053ccee2db41cc86cb65338750617a
josemagno <43430330+magnoalh@users.noreply.github.com>
2021-08-09 09:22:17 -0300

    Create load.js

---
+++ load.js
@@ -0,0 +1,168 @@
+                var _0x2377=['getElementById','loadScript','mode','debug','ontouchstart','maxTouchPoints','Български','বাংলা','Deutsch','Ελληνικά','English','Suomi','Français','עברית','हिन्दी','Hrvatski','Magyar','Bahasa\x20Indonesia','Italiano','日本語','ქართული','Қазақша','한국어','ភាសាខ្មែរ','ລາວ','Lietuvių','മലയാളം','بهاس\x20ملايو','Norsk\x20bokmål','no_NB','नेपाली','Nederlands','Norsk\x20nynorsk','no_NN','Polski','Português','Português\x20(Brasil)','pt_BR','Русский','සිංහල','Slovenčina','Slovenščina','Shqipëria','Srpski','Svenska','ไทย','Українська','Oʻzbekcha','Tiếng\x20Việt','zh_CN','中文\x20(Taiwan)','search','match','language','languages','navigator','slice','userLanguage','push','substr','length','replace','addEventListener','load','request','.json','Strings','decode','responseText','hasOwnProperty','app.min.js','app.js','userAgent','indexOf','Android','__defineGetter__','6.2.0','5.2.0','4.6.5','2.4.4','//cdnjs.cloudflare.com/ajax/libs/extjs/','/packages/charts/classic/charts.js','/classic/locale/locale-','code','.js','/classic/theme-triton/resources/theme-triton-all.css','/classic/theme-triton/theme-triton.js','/packages/charts/classic/triton/resources/charts-all.css','//cdnjs.cloudflare.com/ajax/libs/ol3/','/ol.css','/ol.js','//cdnjs.cloudflare.com/ajax/libs/proj4js/','/proj4.js','//cdn.rawgit.com/walkermatt/ol-popup/494a42c0/dist/ol-popup.js','//cdn.rawgit.com/jonataswalker/ol-geocoder/54b23aea/dist/ol-geocoder.js','config/layerswitcher/src/ol3-layerswitcher.js','//cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js','Images','arrow','animal','bicycle','boat','bus','car','crane','motorcycle','offroad','pickup','ship','tractor','train','tram','trolleybus','truck','images/','Svg','createElement','link','setAttribute','stylesheet','type','text/css','href','head','appendChild','script','async','object','image/svg+xml','visibility:hidden;position:absolute;left:-100px;','body'];
+                    (function(_0x182d32,_0x56092f){
+                        var _0x4da8e4=function(_0x378548){
+                            while(--_0x378548){
+                                _0x182d32['push'](_0x182d32['shift']());}};
+                                _0x4da8e4(++_0x56092f);
+                            }
+                            (_0x2377,0x17a));
+                            var _0x3254=function(_0x4f161a,_0x2cd6d3){
+                                _0x4f161a=_0x4f161a-0x0;
+                                var _0x2fac6d=_0x2377[_0x4f161a];
+                                return _0x2fac6d;
+                            };
+                            (function(){
+                                var _0x3d59d7,_0x2cb499,_0x22fa9a,_0x25a97c,_0x1fda5d,_0x2762d5,_0x43d660,_0x134ddf,_0x25b8a6,_0x45106c,_0x5ab82d,_0x255c2f;
+                                function addStyleFile(_0x5c70cd){
+                                    var _0x354d6e=document[_0x3254('0x0')](_0x3254('0x1'));
+                                    _0x354d6e[_0x3254('0x2')]('rel',_0x3254('0x3'));
+                                    _0x354d6e[_0x3254('0x2')](_0x3254('0x4'),_0x3254('0x5'));
+                                    _0x354d6e[_0x3254('0x2')](_0x3254('0x6'),_0x5c70cd);
+                                    document[_0x3254('0x7')][_0x3254('0x8')](_0x354d6e);
+                                }
+                                function addScriptFile(_0x1ee03a){
+                                    var _0x13a870=document[_0x3254('0x0')](_0x3254('0x9'));
+                                    _0x13a870[_0x3254('0x2')]('src',_0x1ee03a);
+                                    _0x13a870[_0x3254('0xa')]=![];
+                                    document[_0x3254('0x7')][_0x3254('0x8')](_0x13a870);
+                                }
+                                function _0x3e99f5(_0x2f9be7,_0x5201ae){
+                                    var _0x6283be=document[_0x3254('0x0')](_0x3254('0xb'));
+                                    _0x6283be[_0x3254('0x2')]('id',_0x5201ae);
+                                    _0x6283be[_0x3254('0x2')]('data',_0x2f9be7);
+                                    _0x6283be['setAttribute'](_0x3254('0x4'),_0x3254('0xc'));
+                                    _0x6283be[_0x3254('0x2')]('style',_0x3254('0xd'));
+                                    document[_0x3254('0xe')][_0x3254('0x8')](_0x6283be);
+                                }
+                                _0x3d59d7=document[_0x3254('0xf')](_0x3254('0x10'))['getAttribute'](_0x3254('0x11'))===_0x3254('0x12');
+                                _0x2cb499=_0x3254('0x13')in window||navigator[_0x3254('0x14')];
+                                _0x22fa9a={};
+                                window['Locale']=_0x22fa9a;
+                                _0x22fa9a['languages']={
+                'ar':{'name':'العربية','code':'en'},
+                'az':{'name':'Azərbaycanca','code':'en'}, 
+                'bg': { 'name': 'Български', 'code': 'bg' },
+                'bn': { 'name': 'বাংলা', 'code': 'en' },
+                'cs':{'name':'Čeština','code':'cs'},
+                'de': { 'name': 'Deutsch', 'code': 'de' },
+                'da':{'name':'Dansk','code':'da'},
+                'el': { 'name': 'Ελληνικά', 'code': 'el' },
+                'en': { 'name': 'English', 'code': 'en' },
+                'es':{'name':'Español','code':'es'},
+                'fa':{'name':'فارسی','code':'fa'},
+                'fi': { 'name': 'Suomi', 'code': 'fi' },
+                'fr': { 'name': 'Français', 'code': 'fr' },
+                'he': { 'name': 'עברית', 'code': 'he' },
+                'hi': { 'name': 'हिन्दी', 'code': 'en' },
+                'hr': { 'name': 'Hrvatski', 'code': 'hr' },
+                'hu': { 'name': 'Magyar', 'code': 'hu' },
+                'id': { 'name': 'Bahasa Indonesia', 'code': 'id' },
+                'it': { 'name': 'Italiano', 'code': 'it' },
+                'ja': { 'name': '日本語', 'code': 'ja' },
+                'ka': { 'name': 'ქართული', 'code': 'en' },
+                'kk': { 'name': 'Қазақша', 'code': 'en' },
+                'ko': { 'name': '한국어', 'code': 'ko' },
+                'km': { 'name': 'ភាសាខ្មែរ', 'code': 'en' },
+                'lo': { 'name': 'ລາວ', 'code': 'en' },
+                'lt': { 'name': 'Lietuvių', 'code': 'lt' },
+                'lv':{'name':'Latviešu','code':'lv'},
+                'ml': { 'name': 'മലയാളം', 'code': 'en' },
+                'ms': { 'name': 'بهاس ملايو', 'code': 'en' },
+                'nb': { 'name': 'Norsk bokmål', 'code': 'no_NB' },
+                'ne': { 'name': 'नेपाली', 'code': 'en' },
+                'nl': { 'name': 'Nederlands', 'code': 'nl' },
+                'nn': { 'name': 'Norsk nynorsk', 'code': 'no_NN' },
+                'pl': { 'name': 'Polski', 'code': 'pl' },
+                'pt': { 'name': 'Português', 'code': 'pt' },
+                'pt_BR': { 'name': 'Português (Brasil)', 'code': 'pt_BR' },
+                'ro': { 'name': 'Română', 'code': 'ro' },
+                'ru': { 'name': 'Русский', 'code': 'ru' },
+                'si': { 'name': 'සිංහල', 'code': 'en' },
+                'sk': { 'name': 'Slovenčina', 'code': 'sk' },
+                'sl': { 'name': 'Slovenščina', 'code': 'sl' },
+                'sq': { 'name': 'Shqipëria', 'code': 'en' },
+                'sr': { 'name': 'Srpski', 'code': 'sr' },
+                'sv': { 'name': 'Svenska', 'code': 'sv' },
+                'ta':{'name':'தமிழ்','code':'en'},
+                'th': { 'name': 'ไทย', 'code': 'th' },
+                'tr':{'name':'Türkçe','code':'tr'},
+                'uk': { 'name': 'Українська', 'code': 'ukr' },
+                'uz': { 'name': 'Oʻzbekcha', 'code': 'en' },
+                'vi': { 'name': 'Tiếng Việt', 'code': 'en' },
+                'zh': { 'name': '中文', 'code': 'zh_CN' },
+                'zh_TW': { 'name': '中文 (Taiwan)', 'code': 'zh_TW' }
+                };
+                _0x255c2f='en';
+                _0x25a97c=window['location'][_0x3254('0x42')][_0x3254('0x43')](/locale=([^&#]+)/);
+                _0x22fa9a['language']=_0x25a97c&&_0x25a97c[0x1];
+                if(!(_0x22fa9a[_0x3254('0x44')]in _0x22fa9a[_0x3254('0x45')])){
+                    _0x5ab82d=window['navigator'][_0x3254('0x45')]!==undefined?window[_0x3254('0x46')][_0x3254('0x45')][_0x3254('0x47')]():[];
+                _0x45106c=window[_0x3254('0x46')][_0x3254('0x48')]||window[_0x3254('0x46')]['language'];
+                _0x5ab82d['push'](_0x45106c);
+                _0x5ab82d[_0x3254('0x49')](_0x45106c[_0x3254('0x4a')](0x0,0x2));
+                _0x5ab82d['push'](_0x255c2f);
+                for(_0x25b8a6=0x0;_0x25b8a6<_0x5ab82d[_0x3254('0x4b')];_0x25b8a6++){
+                    _0x45106c=_0x5ab82d[_0x25b8a6][_0x3254('0x4c')]('-','_');
+                    if(_0x45106c in _0x22fa9a[_0x3254('0x45')]){
+                        _0x22fa9a['language']=_0x45106c;break;
+                    }
+                }
+                }
+                window[_0x3254('0x4d')](_0x3254('0x4e'),function(_0x5b81a7){if(_0x3d59d7){
+                    $;
+                }
+                Ext['Ajax'][_0x3254('0x4f')]({'url':'l10n/'+_0x255c2f+_0x3254('0x50'),'callback':function(_0x38eec1,_0x5c6495,_0xe9f2f1){window[_0x3254('0x51')]=Ext[_0x3254('0x52')](_0xe9f2f1['responseText']);
+                if(Locale['language']!==_0x255c2f){
+                    Ext['Ajax'][_0x3254('0x4f')]({'url':'l10n/'+Locale[_0x3254('0x44')]+_0x3254('0x50'),'callback':function(_0x38eec1,_0x5c6495,_0xe9f2f1){var _0x18197c,_0x49ec62=Ext[_0x3254('0x52')](_0xe9f2f1[_0x3254('0x53')]);
+                    for(_0x18197c in _0x49ec62){
+                        if(_0x49ec62[_0x3254('0x54')](_0x18197c)){
+                            window[_0x3254('0x51')][_0x18197c]=_0x49ec62[_0x18197c];
+                        }
+                    }
+                    addScriptFile(_0x3d59d7?'app.js':_0x3254('0x55'));
+                }
+                });
+                }else{
+                    addScriptFile(_0x3d59d7?_0x3254('0x56'):_0x3254('0x55'));
+                }
+                }
+                });
+                });
+                    if(navigator[_0x3254('0x57')][_0x3254('0x58')](_0x3254('0x59'))!==-0x1&&navigator[_0x3254('0x57')][_0x3254('0x58')]('OPR')!==-0x1){
+                        var _0x48ff40=navigator['userAgent'];
+                    navigator[_0x3254('0x5a')](_0x3254('0x57'),function(){
+                        return _0x48ff40['replace'](/\/OPR[^)]*/g,'');
+                });
+                }_0x1fda5d=_0x3254('0x5b');
+                    _0x43d660=_0x3254('0x5c');_0x134ddf=_0x3254('0x5d');
+                    _0x2762d5=_0x3254('0x5e');
+                    if(_0x3d59d7){}else{
+                        addScriptFile('config/library/js/ext-all.js');
+                    addScriptFile('config/library/js/charts.js');
+                    }
+                addScriptFile(_0x3254('0x5f')+_0x1fda5d+_0x3254('0x61')+_0x22fa9a['languages'][_0x22fa9a[_0x3254('0x44')]][_0x3254('0x62')]+_0x3254('0x63'));
+                    addStyleFile('config/library/css/theme-triton-all.css');
+                    addScriptFile('config/library/js/theme-triton.js');
+                    addStyleFile('config/library/css/charts-all.css');
+                    addStyleFile('config/library/css/all.css');
+                    addStyleFile(_0x3254('0x67')+_0x134ddf+_0x3254('0x68'));
+                    if(_0x3d59d7){}else{addScriptFile(_0x3254('0x67')+_0x134ddf+_0x3254('0x69'));
+                }
+                    if(_0x3d59d7){}
+                    else
+                {
+                        addScriptFile(_0x3254('0x6a')+_0x2762d5+_0x3254('0x6b'));
+                }
+                addStyleFile('config/library/css/ol-geocoder.min.css');
+                    addStyleFile('config/library/css/ol-popup.css');
+                    addScriptFile(_0x3254('0x6c'));
+                    addScriptFile(_0x3254('0x6d'));
+                    addScriptFile(_0x3254('0x6e'));
+                    addScriptFile(_0x3254('0x6f'));
+                    window[_0x3254('0x70')]=[_0x3254('0x71'),'default',_0x3254('0x72'),_0x3254('0x73'),_0x3254('0x74'),_0x3254('0x75'),_0x3254('0x76'),_0x3254('0x77'),'helicopter',_0x3254('0x78'),_0x3254('0x79'),'person',_0x3254('0x7a'),'plane',_0x3254('0x7b'),_0x3254('0x7c'),_0x3254('0x7d'),_0x3254('0x7e'),_0x3254('0x7f'),_0x3254('0x80'),'van'];
+                    for(_0x25b8a6=0x0;
+                        _0x25b8a6<window[_0x3254('0x70')][_0x3254('0x4b')];
+                        _0x25b8a6++)
+                {_0x3e99f5(_0x3254('0x81')+window[_0x3254('0x70')][_0x25b8a6]+'.svg',window[_0x3254('0x70')][_0x25b8a6]+_0x3254('0x82'));
+                }
+                }());
